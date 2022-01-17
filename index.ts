3e6e51c3007febf2ddcb485163a94344eba67db9
Gary Zhang <gzhang@primeobjects.com>
2022-01-17 12:22:03 -0800

    remove _window

--- index.ts
+++ index.ts
@@ -6,7 +6,6 @@
 import Constants from './libs/constants';
 import Colors from './libs/colors';
 
-export const _window: any = typeof window !== "undefined" ? window : {};
 export const _process: any = typeof process !== "undefined" ? process : {};;
 export const _track = `${_process?.env?.TRACK}`.toLowerCase() == 'true';
 
